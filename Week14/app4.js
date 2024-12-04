c2a95ce4e9b372a7d270baad93482da7bad1f063
Aio1135 <161312300+Aio1135@users.noreply.github.com>
2024-12-04 17:36:17 +0900

    Create app4.js

---
+++ Week14/app4.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.status(200).send('<h1>Welocome!</h1>');
+});
+
+app.get('/about',(req,res)=>{
+    res.status(200).send('<h2>Here is SSM home.</h2>');
+});
+
+app.listen(3000);
